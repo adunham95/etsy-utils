f32d162747ba423ae0934e6eaf78decfa56c13a6
Adrian <adunham95@gmail.com>
2024-12-16 19:25:18 -0500

    Intial Commit

---
+++ src/routes/description/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let tagText = $state('');
+
+	let tags = $derived(tagText.split(',').join(' | '));
+</script>
+
+<textarea bind:value={tagText} />
+
+<p>
+	{tags}
+	Modern Christian Art | Modern Scripture | Christian Home Decor | Bible Quote Print | Modern Bible Verse
+	| Minimal Aesthetic | Bible Verse Wall Art | Minimal Home Decor | Christian Printables | Scripture
+	Wall Art
+</p>
+<p>_________________________________________________</p>
+<p>
+	Aesthetic Christian wall art perfect for you bedroom, living room, office, or as a gift! Psalm 23,
+	"The Lord is my shepherd, I shall not want..." Shop my other prints here! :
+	https://selahprintstudio.etsy.com PURCHASE INCLUDES -5 high resolution files in different ratios
+	to fit different frame sizes: ·FILE 1 - 2:3 ratio for printing: inch - 6x9, 12x18, 20x30, 24x36 cm
+	- 10x15, 20x30, 30x45, 40x60 ·FILE 2 - 3:4 ratio for printing: inch - 6x8, 12x16, 18x24 cm -
+	15x20, 22x30, 30x40 ·FILE 3 - 4:5 ratio for printing: inch - 4x5, 8x10, 16x20 cm - 20x25, 40x50
+	·FILE 4- 1:1 ratio for printing: inch - sizes up to 20x20 cm - sizes up to 50x50 cm ·FILE 5- 11x14
+	file for printing these sizes: inch - 11x14 cm - 22x28 ·Custom size: if you need a size not listed
+	here, please contact us! HOW TO DOWNLOAD After purchasing, you will be redirected to the download
+	page. Click 'download' to save the files on your device. Visit this link more helpful information:
+	https://help.etsy.com/hc/en-us/articles/115013328108?segment=shopping HOW TO PRINT Determine which
+	file you need to use according to the size you want the print to be. Then download and use the
+	file to print at home, at a local or online print center (such as Costco, Walgreens, Shutterfly).
+	FRAME IT & ENJOY Frame this aesthetic poster and hang it in your bedroom, living room, office, or
+	give it as a gift! Be reminded of God's truth and love every time you see it. PLEASE NOTE: ·This
+	product is a DIGITAL DOWNLOAD only. No physical item will be shipped to you. ·Slight color
+	variations could be a result of your monitor display, printer, print paper, and ink used. ·Due to
+	the nature of digital downloads, I do not accept returns, exchanges, or cancellations. If you have
+	any issues or questions with your order, please contact us! Thank you for visiting Selah Print
+	Studio. ! Christian Wall Art | Minimal Christian Home Decor | Baptism Gift | Instant Download |
+	Digital Download | Modern Scripture Decor | Minimal Bible Verse Prints | Typography Wall Art |
+	Christian Print Gift | Aesthetic Christian Prints COPYRIGHT ©SelahPrintStudio. This design is for
+	PERSONAL USE ONLY. You may not share, distribute, or resell product.
+</p>
---
+++ src/routes/download-file/+page.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	let fileNames: string[] = [];
+
+	function handleFileChange(event: Event): void {
+		const input = event.target as HTMLInputElement;
+		if (!input.files) return; // Ensure `files` exists
+		const files: FileList = input.files;
+		console.log({ files });
+		fileNames = Array.from(files).map((file: File) => file.name); // Extract file names
+	}
+
+	function getFileSize(string: string) {
+		const [fileName, size, extension] = string.split('.');
+		console.log({ fileName, size, extension });
+		return size;
+	}
+
+	function getFileSizes(size: string) {
+		if (Object.keys(fileSizes).includes(size)) {
+			return fileSizes[size as keyof typeof fileSizes];
+		} else return 'error';
+	}
+
+	const fileSizes = {
+		'2:3': '4x6, 6x9, 8x12, 21x18, 16x24, 20x30 and 24x36',
+		'3:4': '6x8, 9x12, 12x16, 15x20, 18x24',
+		'4:5': '4x5, 8x10, 16x20',
+		'11:14': '11x114'
+	};
+</script>
+
+<div class="hide-on-print flex justify-between p-2">
+	<div>
+		<label>
+			Upload files (use filename format, NAME.RATIO:RATIO.png):
+			<input type="file" multiple onchange={handleFileChange} />
+		</label>
+		{#if fileNames.length > 0}
+			<h3>Uploaded Files:</h3>
+			<ul>
+				{#each fileNames as name}
+					<!-- {@const size = } -->
+					<li>{name}</li>
+				{/each}
+			</ul>
+		{/if}
+	</div>
+
+	<div class="">
+		<button
+			onclick={() => window.print()}
+			class="m-1 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Save as PDF</button
+		>
+		<button
+			onclick={() => (fileNames = [])}
+			class="m-1 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Clear</button
+		>
+	</div>
+</div>
+
+<main class="px-2">
+	<div class="flex justify-center">
+		<img class="max-w-[200px]" src="/logo.png" alt="logo" />
+	</div>
+	<div class="flex justify-center py-2">
+		<h1 class="text-3xl font-bold tracking-tight text-gray-900">
+			Thank you so much for your purchase!
+		</h1>
+	</div>
+
+	<div class="text-md mt-8 space-y-2 text-pretty font-medium text-gray-700 sm:text-xl/8">
+		<p>We sincerely hope you enjoy these printable wall art files.</p>
+
+		<p>
+			You will find the following digital files in the .zip folder you just downloaded from Etsy
+		</p>
+
+		<ul class="">
+			{#each fileNames as name}
+				<li>{name} ratio file (use to print{getFileSizes(getFileSize(name))})</li>
+			{/each}
+		</ul>
+
+		<p>Steps to get started</p>
+		<ol class="">
+			<li>
+				1. Open .zip folder and select the file you’d like to use (choose correct ratio size
+				depending on what size print you’d like).
+			</li>
+			<li>
+				2. Decide on the printer or printing service you’d like to use. Feel free to print at home
+				using a desktop printer or choose a professional print service to order from.
+			</li>
+			<li>
+				3. If printing from a desktop printer from a desktop printer, click on the selected PDF
+				document and print the document making sure to match the printer setting to the size of
+				paper you are using. If using a video professional print service, place an order using the
+				correct file that you selected from the .zip file.
+			</li>
+		</ol>
+	</div>
+</main>
+
+<style>
+	label {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+	}
+</style>
