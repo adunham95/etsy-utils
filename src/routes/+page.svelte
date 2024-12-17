a2dbac5d1ab1c31391f3c0f528e14b15f38f1a0a
Adrian <adunham95@gmail.com>
2024-12-16 19:33:00 -0500

    Added home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<div class=" p-10">
+	<div
+		class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0"
+	>
+		<div
+			class="group relative rounded-lg bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 sm:rounded-tr-none"
+		>
+			<!-- <div>
+				<span class="inline-flex rounded-lg bg-teal-50 p-3 text-teal-700 ring-4 ring-white">
+					<svg
+						class="size-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+						data-slot="icon"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/>
+					</svg>
+				</span>
+			</div> -->
+			<div class="mt-8">
+				<h3 class="text-base font-semibold text-gray-900">
+					<a href="/download-file" class="focus:outline-none">
+						<!-- Extend touch target to entire panel -->
+						<span class="absolute inset-0" aria-hidden="true"></span>
+						Create Download File
+					</a>
+				</h3>
+				<p class="mt-2 text-sm text-gray-500">
+					Upload images and will auto generate the file for download instuctions
+				</p>
+			</div>
+			<span
+				class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+				aria-hidden="true"
+			>
+				<svg class="size-6" fill="currentColor" viewBox="0 0 24 24">
+					<path
+						d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
+					/>
+				</svg>
+			</span>
+		</div>
+		<div
+			class="group relative rounded-lg bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 sm:rounded-tr-none"
+		>
+			<!-- <div>
+				<span class="inline-flex rounded-lg bg-teal-50 p-3 text-teal-700 ring-4 ring-white">
+					<svg
+						class="size-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+						data-slot="icon"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/>
+					</svg>
+				</span>
+			</div> -->
+			<div class="mt-8">
+				<h3 class="text-base font-semibold text-gray-900">
+					<a href="/description" class="focus:outline-none">
+						<!-- Extend touch target to entire panel -->
+						<span class="absolute inset-0" aria-hidden="true"></span>
+						Description Generator
+					</a>
+				</h3>
+				<p class="mt-2 text-sm text-gray-500">
+					Fill in the tags and description and it will autogenerate the Etsy description
+				</p>
+			</div>
+			<span
+				class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+				aria-hidden="true"
+			>
+				<svg class="size-6" fill="currentColor" viewBox="0 0 24 24">
+					<path
+						d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
+					/>
+				</svg>
+			</span>
+		</div>
+	</div>
+</div>
